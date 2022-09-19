bc2056075ed6a265ce630b46a032c883c49e273f
Bassem Mohamed <bkamal@beyond-creation.net>
2022-09-19 15:53:46 +0200

    edit react pagination

--- pages/paginationPage.js
+++ pages/paginationPage.js
@@ -1,34 +1,69 @@
 import { useEffect, useState } from "react";
+import ReactPaginate from 'react-paginate';
 
 
 const paginationPage = () => {
   const [page,setPage]=useState(1)
   const [data, setData] = useState([])
+
   const fetchHandler = async (pages) => {
     await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=9f88bf74a43bfc37f66724aa8369bbe1&page=${pages}`)
       .then((res) => res.json())
-      .then((data) => setData(data.results));
-  };
+      .then((data) =>    
+      setData(data),
+  )};
+
   useEffect(() => {
     fetchHandler(page);
   }, [page]);
-  console.log(data[0]?.id);
-  const arr=[1,2,3,4,5,6]
+
+  const handlePage = (e) => {
+    setPage(e.selected+1)
+  } 
+  // const handlePage = (e) => {
+  //   setPage(e.selected+1)
+  // }
   return (
     <>
-    {data.map((data,index)=>(
-
-    <div key={index}>
-      {data.id}
+    <div className="data">
+      <ul className="data-list">
+        {data?.results?.map((data,index)=>(
+        <li className="data-item"  key={index}>
+          {data.id}
+        </li>
+        ))}
+        {data.page}
+      </ul>
     </div>
-    ))}
-    {arr.map((number,index)=>(
+    {/* {arr.map((number,index)=>(
       <div onClick={()=>{setPage(number)}}>
         {number}
       </div>
-    ))}
-    <div onClick={()=>{setPage(++page)}}>next</div>
-    <div onClick={()=>{setPage(--page)}}>back</div>
+    ))} */} 
+    <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePage}
+        pageRangeDisplayed={5}
+        pageCount={500}
+        previousLabel="< previous"
+        renderOnZeroPageCount={null}
+      />
+    {/* <div className="pagination">
+      <div className="btn-next" onClick={()=>{setPage(++page)}}>next</div>
+      {data?.results?.map((item, index) => ( 
+          index!=0&& 
+          <button
+            key={index}
+            className={`${index === page ? 'active-btn' : null}`}
+            onClick={() => handlePage(index)}
+          >
+            {index }
+          </button>
+        
+      ))}
+      <div className="btn-back" onClick={()=>{setPage(--page)}}>back</div>
+    </div> */}
     </>
   );
 }
